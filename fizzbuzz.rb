ee5c01fdbc03b5b72bb2073b187cb823b9d2b7ad
Roman Pirag <mr.anadale@yahoo.com>
2018-10-19 22:48:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  
-  elsif num % 5 == 0 
+  if num % 5 == 0 
     return "Buzz"
   elsif num % 3 == 0 
     return "Fizz"
-    
-    
+  elsif num % 3 && num % 5 == 0 
+    return "FizzBuzz"
   else return nil 
   end
 end
